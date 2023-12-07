fba3e157ec40328ef30a536e5b54ec73a91705d6
Jenin82 <jenin8282@gmail.com>
2023-12-07 23:52:35 +0530

    skills in progress

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
     Backend_skill,
     Frontend_skill,
@@ -8,6 +10,9 @@ import {
 import React from "react";
 import SkillDataProvider from "../sub/SkillDataProvider";
 import SkillText from "../sub/SkillText";
+import { InView } from "react-intersection-observer";
+import { slideInFromLeft, slideInFromRight } from "@/utils/motion";
+import { motion } from "framer-motion";
 
 const Skills = () => {
     return (
@@ -18,64 +23,71 @@ const Skills = () => {
         >
             <SkillText />
 
-            <div className="flex flex-row justify-around flex-wrap mt-3 gap-5 items-center">
-                {Skill_data.map((image, index) => (
-                    <SkillDataProvider
-                        key={index}
-                        src={image.Image}
-                        width={image.width}
-                        height={image.height}
-                        index={index}
-                    />
-                ))}
+            <div className="flex flex-col items-center justify-center w-[95%] gap-4">
+                <div className="w-full flex flex-col lg:flex-row items-center justify-between gap-4">
+                    <div className="w-full lg:w-1/2 h-full">
+                        <InView triggerOnce={false}>
+                            {({ inView, ref }) => (
+                                <motion.div
+                                    ref={ref}
+                                    initial="hidden"
+                                    animate={inView ? "visible" : "hidden"}
+                                    variants={slideInFromLeft(0.5)}
+                                    className="rounded-md text-[white] w-full my-auto py-[8px] px-[10px] border border-[#7042f88b] opacity-[0.9]"
+                                >
+                                    <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 text-2xl font-bold">
+                                        Frontend{" "}
+                                    </span>
+                                    <br />
+                                    <div className="flex flex-row justify-around flex-wrap my-4 gap-5 items-center">
+                                        {Frontend_skill.map((image, index) => (
+                                            <SkillDataProvider
+                                                key={index}
+                                                src={image.Image}
+                                                width={image.width}
+                                                height={image.height}
+                                                index={index}
+                                            />
+                                        ))}
+                                    </div>
+                                </motion.div>
+                            )}
+                        </InView>
+                    </div>
+                    <div className="w-full lg:w-1/2 h-full">
+                        <InView triggerOnce={false}>
+                            {({ inView, ref }) => (
+                                <motion.div
+                                    ref={ref}
+                                    initial="hidden"
+                                    animate={inView ? "visible" : "hidden"}
+                                    variants={slideInFromRight(0.5)}
+                                    className="rounded-md text-[white] w-full h-full py-[8px] px-[10px] border border-[#7042f88b] opacity-[0.9]"
+                                >
+                                    <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 text-2xl font-bold">
+                                        Backend{" "}
+                                    </span>
+                                    <br />
+                                    <div className="flex flex-row justify-around flex-wrap my-4 gap-5 items-center">
+                                        {Backend_skill.map((image, index) => (
+                                            <SkillDataProvider
+                                                key={index}
+                                                src={image.Image}
+                                                width={image.width}
+                                                height={image.height}
+                                                index={index}
+                                            />
+                                        ))}
+                                    </div>
+                                </motion.div>
+                            )}
+                        </InView>
+                    </div>
+                </div>
+                
             </div>
 
-            <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-                {Frontend_skill.map((image, index) => (
-                    <SkillDataProvider
-                        key={index}
-                        src={image.Image}
-                        width={image.width}
-                        height={image.height}
-                        index={index}
-                    />
-                ))}
-            </div>
-            <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-                {Backend_skill.map((image, index) => (
-                    <SkillDataProvider
-                        key={index}
-                        src={image.Image}
-                        width={image.width}
-                        height={image.height}
-                        index={index}
-                    />
-                ))}
-            </div>
-            <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-                {Full_stack.map((image, index) => (
-                    <SkillDataProvider
-                        key={index}
-                        src={image.Image}
-                        width={image.width}
-                        height={image.height}
-                        index={index}
-                    />
-                ))}
-            </div>
-            <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-                {Other_skill.map((image, index) => (
-                    <SkillDataProvider
-                        key={index}
-                        src={image.Image}
-                        width={image.width}
-                        height={image.height}
-                        index={index}
-                    />
-                ))}
-            </div>
-
-            <div className="w-full h-full absolute">
+            <div className="hidden md:block w-full h-full absolute">
                 <div className="w-full h-full z-[-10] opacity-30 absolute flex items-center justify-center bg-cover">
                     <video
                         className="w-full h-auto"
--- components/sub/SkillText.tsx
+++ components/sub/SkillText.tsx
@@ -2,27 +2,46 @@
 import React from "react";
 import { motion } from "framer-motion";
 import {
-    slideInFromLeft,
+	slideInFromLeft,
     slideInFromRight,
     slideInFromTop,
 } from "@/utils/motion";
-import { BsStars } from "react-icons/bs";
+import { InView } from "react-intersection-observer";
 
 const SkillText = () => {
     return (
         <div className="w-full h-auto flex flex-col items-center justify-center">
-            <motion.div
-                variants={slideInFromLeft(0.5)}
-                className="text-[30px] text-white font-medium mt-[10px] text-center mb-[15px]"
-            >
-                Making apps with modern technologies
-            </motion.div>
-            <motion.div
-                variants={slideInFromRight(0.5)}
-                className="cursive text-[20px] text-gray-200 mb-10 mt-[10px] text-center"
-            >
-                Never miss a task, deadline or idea
-            </motion.div>
+            <InView triggerOnce={false}>
+                {({ inView, ref }) => (
+                    <motion.div
+                        ref={ref}
+                        initial="hidden"
+                        animate={inView ? "visible" : "hidden"}
+                        variants={slideInFromTop}
+                        className="text-[40px] pt-[5rem] pb-3 md:p-0 font-medium text-center text-gray-200 z-50"
+                    >
+                        My
+                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+                            {" "}
+                            Skills{" "}
+                        </span>
+                    </motion.div>
+                )}
+            </InView>
+
+            <InView triggerOnce={false}>
+                {({ inView, ref }) => (
+                    <motion.div
+                        ref={ref}
+                        initial="hidden"
+                        animate={inView ? "visible" : "hidden"}
+                        variants={slideInFromLeft(0.5)}
+                        className="cursive text-[20px] text-gray-200 mb-10 mt-[10px] text-center"
+                    >
+                        Never miss a task, deadline or idea
+                    </motion.div>
+                )}
+            </InView>
         </div>
     );
 };
