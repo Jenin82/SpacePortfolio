b126aba9220eeb4c1050e86bd914c8293c813392
Jenin82 <jenin8282@gmail.com>
2023-12-10 20:58:51 +0530

    temp fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
                 <StarsCanvas />
                 <Navbar />
                 {children}
-                <Footer />
+                {/* <Footer /> */}
             </body>
         </html>
     );
